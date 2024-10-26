549eed6de6e41bc3e8d5f45b7a8b71fef6ab2d97
namxle <103374580+namxle@users.noreply.github.com>
2024-10-26 21:11:38 +0700

    feat: add more comments

--- data_preprocessing_explicit_valence.py
+++ data_preprocessing_explicit_valence.py
@@ -18,49 +18,47 @@ df_drugs_smiles = pd.read_csv(f'{data_dir}/drug_smiles.csv')
 DRUG_TO_INDX_DICT = {drug_id: indx for indx, drug_id in enumerate(df_drugs_smiles['drug_id'])}
 
 # drug_id_mol_graph_tup (list of tuple): Contains drug information of all drugs as a list of tuples.
-# Each tuple has the following elements:
-# - id: Drug ID provided in drug_smiles.csv
-# - mol_graph: Rdkit Mol object extracted from Smiles string
-# Note: Smiles object only represent the connectivity of atoms in a molecule in a text form.
-#       Where Rdkit Mol object capture a complete graph-based representation of molecule.
-#       Making it easier for feature extraction (atomic features, bond features, ...)
+# each tuple has the following elements:
+# - id: drug ID provided in drug_smiles.csv
+# - mol_graph: rdkit Mol object extracted from Smiles string
+# note: smiles object only represent the connectivity of atoms in a molecule in a text form.
+#       where Rdkit Mol object capture a complete graph-based representation of molecule.
+#       making it easier for feature extraction (atomic features, bond features, ...)
 drug_id_mol_graph_tup = [(id, Chem.MolFromSmiles(smiles.strip())) for id, smiles in zip(df_drugs_smiles['drug_id'], df_drugs_smiles['smiles'])]
-
-# Gettings information and features of atoms
 
 # ATOM_MAX_NUM (int): Max number of atoms in every drugs in drug_id_mol_graph_tup list.
 ATOM_MAX_NUM = np.max([m[1].GetNumAtoms() for m in drug_id_mol_graph_tup])
 
 # AVAILABLE_ATOM_SYMBOLS (list of str): Contains all symbols of all atoms of each drugs in drug_id_mol_graph_tup list.
-# Atoms have symbols like Ag, Na, Br, ...
+# atoms have symbols like Ag, Na, Br, ...
 AVAILABLE_ATOM_SYMBOLS = list({a.GetSymbol() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup)})
 
 # AVAILABLE_ATOM_DEGREES (list of int): Contains all degree of all atoms of all drugs in drug_id_mol_graph_tup list
-# An atom degree is the total number of connections between atoms (Simply it's degree of nodes).
+# an atom degree is the total number of connections between atoms (Simply it's degree of nodes).
 AVAILABLE_ATOM_DEGREES = list({a.GetDegree() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup)})
 
 # AVAILABLE_ATOM_TOTAL_HS (list of int): Contains all Hydrogens attached to the atoms of all drugs in drug_id_mol_graph_tup list
 AVAILABLE_ATOM_TOTAL_HS = list({a.GetTotalNumHs() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup)})
 
 # max_valence (int): A maximum number of Implicit Valence in all atoms of all drugs. Minimum = 9.
-# Implicit Valance: Describes the "wants" of complete bonds. For eg. C=0 wants to complete 4 bonds,
-#                   Because C can form 4 bonds with other atoms or molecule
-#                   Then Implicit Valance = 2.
+# implicit valance: describes the "wants" of complete bonds. For eg. C=0 wants to complete 4 bonds,
+#                   because C can form 4 bonds with other atoms or molecule
+#                   Tthen Implicit Valance = 2.
 max_valence = max(a.GetImplicitValence() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup))
 max_valence = max(max_valence, 9)
 
-# AVAILABLE_ATOM_VALENCE (NumPy array of int): Generates a NumPy array of integers, starting from 0 and ending at value. 
-# The result is an array containing all integers from 0 up to max_valence (but not including max_valence).
+# AVAILABLE_ATOM_VALENCE (NumPy array of int): generates a NumPy array of integers, starting from 0 and ending at value. 
+# the result is an array containing all integers from 0 up to max_valence (but not including max_valence).
 AVAILABLE_ATOM_VALENCE = np.arange(max_valence + 1)
 
 # MAX_ATOM_FC (int): maximum absolute formal charge of all atoms in a list of molecular structures.
-# Formal Charge: Provide a way to estimate ad track electron distribution within a molecule.
+# formal Charge: Provide a way to estimate ad track electron distribution within a molecule.
 MAX_ATOM_FC = abs(np.max([a.GetFormalCharge() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup)]))
 MAX_ATOM_FC = MAX_ATOM_FC if MAX_ATOM_FC else 0
 
 # MAX_RADICAL_ELC (int): calculates the maximum absolute number of radical electrons found across all atoms in a list of molecular structures.
-# Radical Electrons: This is important feature, play importan participation in reactions. 
-#                    This is the key in many chemical.
+# radical electrons: this is important feature, play importan participation in reactions. 
+#                    this is the key in many chemical.
 MAX_RADICAL_ELC = abs(np.max([a.GetNumRadicalElectrons() for a in itertools.chain.from_iterable(m[1].GetAtoms() for m in drug_id_mol_graph_tup)]))
 MAX_RADICAL_ELC = MAX_RADICAL_ELC if MAX_RADICAL_ELC else 0
 
@@ -90,13 +88,13 @@ def atom_features(atom,
     Get all features of an atom.
 
     Args:
-        atom (Atom): Atom to be performed features extractation.
-        explicit_H (boolean): Include Explicit Hydrogens. Default = True.
-        use_chirality (boolean): Include Chirality feature. Default = False.
-        use_explicit_valence (boolean): Include Explicit Valence feature. Default = True.
+        atom (Atom): atom to be performed features extractation.
+        explicit_H (boolean): include Explicit Hydrogens. Default = True.
+        use_chirality (boolean): include Chirality feature. Default = False.
+        use_explicit_valence (boolean): include Explicit Valence feature. Default = True.
 
     Returns:
-        NumPy array: Represents a numerical one hot encoding feature vector of an atom.
+        NumPy array: Represents an one hot encoding feature vector of an atom.
     """
 
     results = one_of_k_encoding_unk(
@@ -111,10 +109,12 @@ def atom_features(atom,
                 Chem.rdchem.HybridizationType.SP3, Chem.rdchem.HybridizationType.
                                     SP3D, Chem.rdchem.HybridizationType.SP3D2
                 ]) + [atom.GetIsAromatic()]
-    # In case of explicit hydrogen(QM8, QM9), avoid calling `GetTotalNumHs`
+    # in case of explicit hydrogen(QM8, QM9), avoid calling `GetTotalNumHs`
     if explicit_H:
         results = results + [atom.GetTotalNumHs()]
 
+    # chilarity: chilarity molecule can have very different properties and biological effects depending on
+    # their orientation. This influence how the molecule interact with biological systems or other chemical entities.
     if use_chirality:
         try:
             results = results + one_of_k_encoding_unk(
@@ -123,7 +123,7 @@ def atom_features(atom,
         except:
             results = results + [False, False
                             ] + [atom.HasProp('_ChiralityPossible')]
-            
+
     if use_explicit_valence:
         results = results + [atom.GetExplicitValence()]
 
@@ -133,31 +133,48 @@ def atom_features(atom,
 
 def get_mol_edge_list_and_feat_mtx(mol_graph):
     """
+    Get molecule edge list and features matrix
+
+    Args:
+        mol_graph (Mol): rdkit mol object of a drug.
+
+    Returns:
+        tuple: edge list and features matrix
     """
+    # features (list of tuple): Contains drug id and features of all atom as a list of tuples.
+    # each tuple has the following elements:
+    # - index: index of an atom.
+    # - features (Numpy array): features of an atom
     features = [(atom.GetIdx(), atom_features(atom)) for atom in mol_graph.GetAtoms()]
-    features.sort() # to make sure that the feature matrix is aligned according to the idx of the atom
+
+    # to make sure that the feature matrix is aligned according to the idx of the atom
+    features.sort()
     _, features = zip(*features)
     features = torch.stack(features)
 
+    # GetBonds(): retrive all bonds in the Mol object. Each bond represent connections between 2 atoms.
+    # for each bond, retrieve the indices of 2 connected by a bond to form a tuple (start_atom_index, end_atom_index)
+    # representing edge between 2 atoms.
+    # convert to a Torch Tensor (2D tensor) of a matrix.
+    # each row represent an edge, the first column and second column being the indices of the bonded atoms.
     edge_list = torch.LongTensor([(b.GetBeginAtomIdx(), b.GetEndAtomIdx()) for b in mol_graph.GetBonds()])
     undirected_edge_list = torch.cat([edge_list, edge_list[:, [1, 0]]], dim=0) if len(edge_list) else edge_list
     
     return undirected_edge_list.T, features
 
-"""
-Notes:
-MOL_EDGE_LIST_FEAT_MTX: dict(drug_id: (edge_list, feature_matrix))
-edge_list: Shape: (num_edges, 2). In the mol object, retrive all bonds. "bonds" means the connection between atoms
--> list of edges tuple(atom1 index, atom2 index)
-undirected_edge_list: Shape: (2, 2 * num_edges). Since molecular bonds are typically undirected, we make each directed edge bidirectional.
--> row1: Contains the start nodes of each edge. row2: Contains the end nodes of each edge.
--> undirected_edge_list.T : Shape (2*num_edges, 2). 2 columns, first col is start node(atom1) and second one is end node(atom2)
-"""
+
+# MOL_EDGE_LIST_FEAT_MTX (dict): each field in the dict is (drug_id: (edge_list, feature_matrix))
+# edge_list (shape: (num_edges, 2)): in the mol object, retrive all bonds. "bonds" means the connection between atoms.
+#           list of edges tuple(atom1 index, atom2 index).
+# undirected_edge_list (shape: (2, 2 * num_edges)): since molecular bonds are typically undirected, we make each directed edge bidirectional.
+#                      first row ontains the start nodes of each edge, second row contains the end nodes of each edge.
+# undirected_edge_list.T (shape (2*num_edges, 2)): 2 columns, first col is start node(atom1) and second one is end node(atom2).
 
 MOL_EDGE_LIST_FEAT_MTX = {drug_id: get_mol_edge_list_and_feat_mtx(mol) 
                                 for drug_id, mol in drug_id_mol_graph_tup}
 MOL_EDGE_LIST_FEAT_MTX = {drug_id: mol for drug_id, mol in MOL_EDGE_LIST_FEAT_MTX.items() if mol is not None}
 
+# TOTAL_ATOM_FEATS (int): total features of an atom.
 TOTAL_ATOM_FEATS = (next(iter(MOL_EDGE_LIST_FEAT_MTX.values()))[1].shape[-1])
 
 
@@ -165,16 +182,29 @@ TOTAL_ATOM_FEATS = (next(iter(MOL_EDGE_LIST_FEAT_MTX.values()))[1].shape[-1])
 df_all_pos_ddi = pd.read_csv(f'{data_dir}/ddis.csv')
 all_pos_tup = [(h, t, r) for h, t, r in zip(df_all_pos_ddi['d1'], df_all_pos_ddi['d2'], df_all_pos_ddi['type'])]
 
-
+# ALL_DRUG_IDS (Numpy array of int): list of all drug ids
 ALL_DRUG_IDS, _ = zip(*drug_id_mol_graph_tup)
 ALL_DRUG_IDS = np.array(list(set(ALL_DRUG_IDS)))
+
+# ALL_TRUE_H_WITH_TR (dict): all drug 1 with drug 2 and their relationship
 ALL_TRUE_H_WITH_TR = defaultdict(list)
+
+# ALL_TRUE_T_WITH_HR (dict): all drug 2 with drug 1 and their relationship
 ALL_TRUE_T_WITH_HR = defaultdict(list)
 
+# FREQ_REL (dict): calculate number of relationships appeared.
 FREQ_REL = defaultdict(int)
+
+# ALL_H_WITH_R (dict): mark the dict relationships with head drug.
 ALL_H_WITH_R = defaultdict(dict)
+
+# ALL_T_WITH_R (dict): mark the dict relationships with tail drug.
 ALL_T_WITH_R = defaultdict(dict)
+
+# ALL_TAIL_PER_HEAD (dict): freaquent of relationships / (length of list drug tail in relationships "r")
 ALL_TAIL_PER_HEAD = {}
+
+# ALL_HEAD_PER_TAIL (dict): freaquent of relationships / (length of list drug head in relationships "r")
 ALL_HEAD_PER_TAIL = {}
 
 for h, t, r in all_pos_tup:
